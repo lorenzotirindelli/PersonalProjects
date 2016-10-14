5badf13c52743b0b74d33d1997eea3d9f2b6dd28
lorenzotirindelli <tsac048@tecnicosuperiorekennedy.it>
2016-10-15 01:22:06 +0200

    no message

--- EuroKart/pages/home/homeController.js
+++ EuroKart/pages/home/homeController.js
@@ -2,6 +2,6 @@ angular.module('euroKart')
     .controller('homeController', function($scope, $state){
 
         $scope.message = 'Ti trovi in home page';
-
+        var provaCommit = 2;
 
     });
